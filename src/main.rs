18a467c2afc9c9862729f772c8aa5a45ae41f5d4
Lev Velykoivanenko <velykoivanenko.lev@gmail.com>
2025-12-29 12:49:12 +0800

    feat: initial implementation to use program as a CLI tool

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
+mod ascent_data;
+mod calculate_ascent;
 mod cli;
+
+use crate::{
+    ascent_data::{AscentSeries, InputTravelData},
+    cli::Cli,
+};
+
+use clap::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    dbg!(&args);
+    let input_travel_data: Vec<InputTravelData> = args
+        .distances_angles
+        .iter()
+        .map(|(distance, angle)| InputTravelData::new(distance, angle, &args.angle_type))
+        .collect();
+    dbg!(&input_travel_data);
+    let ascent_series = AscentSeries::from(input_travel_data);
+    dbg!(&ascent_series);
 }
